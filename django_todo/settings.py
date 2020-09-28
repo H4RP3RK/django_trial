a1a6b890eeefb6cf98ad12b14e71c450225d62d6
H4RP3RK <kirsty.harper@zoho.com>
2020-09-28 11:43:14 +0000

    setup environ vars and auto deploys

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+85n7g%xl2wjvr3-m-abz71d!yq#ixq7jc)tn(5uuk3zgh9!3p'
+SECRET_KEY = os.environ.get('SECRET_KEY', '+85n7g%xl2wjvr3-m-abz71d!yq#ixq7jc)tn(5uuk3zgh9!3p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['h4rp3rk-django-test.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://dtskpehsattrju:8ee87084676874908fcdce9491d7fde0de879c16ddcfe3bcd0ccfccd24694d79@ec2-54-152-40-168.compute-1.amazonaws.com:5432/d5evik4adikrnt')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
